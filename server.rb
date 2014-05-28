7cb5071be1bba09105fcbb258bb13f0fa1b7b8a8
AlexMorgan <alexmorgan.am@gmail.com>
2014-05-28 02:39:02 -0400

    Added testimonials and feature to add more

--- server.rb
+++ server.rb
@@ -1,16 +1,36 @@
+require 'csv'
 require 'sinatra'
 require 'shotgun'
 require 'csv'
 require 'pry'
 
-get '/' do
+def read_from_csv(filename)
+  testimonials = []
+  CSV.foreach(filename, headers: true, header_converters: :symbol) do |row|
+    testimonials << row.to_hash
+  end
+  testimonials
+end
 
+def write_to_csv(filename, name, testimonial)
+  CSV.open(filename, 'a') do |row|
+    row << [name,testimonial]
+  end
+end
+
+#------------------------------------------ Routes ------------------------------------------
+get '/' do
+  @testimonials = read_from_csv('testimonials.csv')
   erb  :index
 end
 
+post '/testimonial' do
+  name = params['name']
+  testimonial = params['testimonial']
 
-get '/about' do
+  write_to_csv('testimonials.csv', name, testimonial)
 
-
-  erb :'about/show'
+  redirect '/'
 end
+
+# http://ididitmyway.herokuapp.com/past/2010/12/4/an_email_contact_form_in_sinatra/ ---- Sinatra Contact Form
